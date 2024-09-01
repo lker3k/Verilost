57bd6f7fd38973e43d81213c7583d8411645feef
Adrian Le <lebanhatminh2004@gmail.com>
2024-09-01 23:31:45 +1000

    minor update

--- Testbenches/bit_counter_tb.sv
+++ Testbenches/bit_counter_tb.sv
@@ -1,15 +1,16 @@
 module bit_counter_tb;
     // Step 1: Define test bench variables and clock:
-    reg   [17:0]      led;
-    reg   [17:0]      whacked; // Inputs as register data type (so we can assign them in the initial block)
+    reg   [17:0]      led_moles;
+    reg   [17:0]      hit_reg;
+    reg         reset; // Inputs as register data type (so we can assign them in the initial block)
     wire  [10:0]      score; // Outputs as wire data type (as they are assigned by the DUT)
 
-    reg clk=0;  // Clock signal for sequential logic
+    reg clk = 0;  // Clock signal for sequential logic
 
     // Step 2: Instantiate Device Under Test:
     bit_counter DUT( // Instantiate the 'Device Under Test' (DUT), an instance of the bit_counter module.
-			.clk(clk),
-        .led(led), .whacked(whacked), // Connect inputs to their respective testbench variables.
+	 .clk(clk),
+        .led_moles(led_moles), .hit_reg(hit_reg), .reset(reset), // Connect inputs to their respective testbench variables.
         .score(score)  // Connect outputs to their respective testbench variables.
     );
     // ^^^ Connects ports of the instantiated module to variables in this module with the same port/variable name.
@@ -21,34 +22,31 @@ module bit_counter_tb;
     initial begin  // Run the following code starting from the beginning of the simulation:
         $dumpfile("waveform.vcd");  // Tell the simulator to dump variables into the 'waveform.vcd' file during the simulation. Required to produce a waveform .vcd file.
         $dumpvars();                // Also required to tell simulator to dump variables into a waveform (with filename specified above).
-
-        repeat(15) begin // Generate random input stimuli (15 times):
+			{reset} = (1);
+			
+        repeat(10) begin // Generate random input stimuli (15 times):
             // Set each input bit to random value (0 or 1):
-            {led} = ($urandom()); // Generate a random unsigned 32-bit value ($urandom), then cast it (2') to a 2-bit value. The inputs led, whacked are then assigned to each bit of this value (in the order: MSB to LSB).
-            // ^^^ We use the LHS concatenation operator here to efficiently assign each of the 2 inputs to a random 1-bit value.
-				{whacked} = (led);
+            {led_moles, hit_reg} = ($urandom()); // Generate a random unsigned 32-bit value ($urandom), then cast it (3') to a 3-bit value. The inputs led_moles, hit_reg, reset are then assigned to each bit of this value (in the order: MSB to LSB).
+            // ^^^ We use the LHS concatenation operator here to efficiently assign each of the 3 inputs to a random 1-bit value.
             // Log input and output values:
-            $display("Inputs: \tled: %b, whacked: %b", led, whacked); // Print inputs to stdout.
+            $display("Inputs: \tled_moles: %b, hit_reg: %b, reset: %b", led_moles, hit_reg, reset); // Print inputs to stdout.
             #10; // Delay for 10 time units to ensure the simulator evaluates the DUT outputs before the next line.
-            $display("Outputs:\tscore: %d", score); // Print outputs to stdout.
+            $display("Outputs:\tscore: %b", score); // Print outputs to stdout.
             $display("======================================");
             #10; // Delay for a further 10 time units to provide a total of 20 time units between input changes (as the clock period is 20 time units).
-            {led} = ($urandom()); // Generate a random unsigned 32-bit value ($urandom), then cast it (2') to a 2-bit value. The inputs led, whacked are then assigned to each bit of this value (in the order: MSB to LSB).
-            // ^^^ We use the LHS concatenation operator here to efficiently assign each of the 2 inputs to a random 1-bit value.
-				{whacked} = (led);
-				$display("Inputs: \tled: %b, whacked: %b", led, whacked); // Print inputs to stdout.
-            #10; // Delay for 10 time units to ensure the simulator evaluates the DUT outputs before the next line.
-            $display("Outputs:\tscore: %d", score); // Print outputs to stdout.
-            $display("======================================");
-            #10; // Delay for a further 10 time units to provide a to
-				{whacked} = ($urandom());
-				$display("Inputs: \tled: %b, whacked: %b", led, whacked); // Print inputs to stdout.
-            #10; // Delay for 10 time units to ensure the simulator evaluates the DUT outputs before the next line.
-            $display("Outputs:\tscore: %d", score); // Print outputs to stdout.
-            $display("======================================");
-            #10; // Delay for a further 10 time units to provide a to
-				
         end
+		  // This is the random bit generation, use to check normal functionality
+		  
+		  repeat(10) begin
+				{led_moles} = ($urandom());
+				{hit_reg} = (led_moles);
+            $display("Inputs: \tled_moles: %b, hit_reg: %b, reset: %b", led_moles, hit_reg, reset); // Print inputs to stdout.
+            #10; // Delay for 10 time units to ensure the simulator evaluates the DUT outputs before the next line.
+            $display("Outputs:\tscore: %b", score); // Print outputs to stdout.
+            $display("======================================");
+            #10; // Delay for a further 10 time
+			end
+			// This is the case where hit all of the moles, use to check the combo logic
 
         $finish(); // Important: must end simulation (or it will go on forever!)
     end
--- Testbenches/integration_tb.sv
+++ Testbenches/integration_tb.sv
@@ -1,19 +1,21 @@
+`timescale 1ns/1ns /* This directive specifies simulation <time unit>/<time precision>. */
 module integration_tb;
     // Step 1: Define test bench variables and clock:
-    reg   [17:0]      led;
-    reg   [17:0]      whacked; // Inputs as register data type (so we can assign them in the initial block)
-    wire  [6:0]      display0;
-    wire  [6:0]      display1;
-    wire  [6:0]      display2;
-    wire  [6:0]      display3; // Outputs as wire data type (as they are assigned by the DUT)
+    reg   [17:0]      SW;
+    reg   [3:0]      KEY; // Inputs as register data type (so we can assign them in the initial block)
+    wire  [17:0]      LEDR;
+    wire  [6:0]      HEX0;
+    wire  [6:0]      HEX1;
+    wire  [6:0]      HEX2;
+    wire  [6:0]      HEX3; // Outputs as wire data type (as they are assigned by the DUT)
 
-    reg clk=0;  // Clock signal for sequential logic
+    reg clk = 0;  // Clock signal for sequential logic
 
     // Step 2: Instantiate Device Under Test:
     integration DUT( // Instantiate the 'Device Under Test' (DUT), an instance of the integration module.
-		.clk(clk),
-        .led(led), .whacked(whacked), // Connect inputs to their respective testbench variables.
-        .display0(display0), .display1(display1), .display2(display2), .display3(display3)  // Connect outputs to their respective testbench variables.
+        .CLOCK_50(clk),
+        .SW(SW), .KEY(KEY), // Connect inputs to their respective testbench variables.
+        .LEDR(LEDR), .HEX0(HEX0), .HEX1(HEX1), .HEX2(HEX2), .HEX3(HEX3)  // Connect outputs to their respective testbench variables.
     );
     // ^^^ Connects ports of the instantiated module to variables in this module with the same port/variable name.
 
@@ -24,15 +26,24 @@ module integration_tb;
     initial begin  // Run the following code starting from the beginning of the simulation:
         $dumpfile("waveform.vcd");  // Tell the simulator to dump variables into the 'waveform.vcd' file during the simulation. Required to produce a waveform .vcd file.
         $dumpvars();                // Also required to tell simulator to dump variables into a waveform (with filename specified above).
-
-        repeat(15) begin // Generate random input stimuli (15 times):
+	
+		  /*
+		  {KEY[0]} = (1);
+        {KEY[3]} = (1);
+        #50;
+        {KEY[3]} = (0);
+        #10;
+		  */
+		  {KEY[0]} = (1);
+        repeat(100) begin // Generate random input stimuli (15 times):
             // Set each input bit to random value (0 or 1):
-            {led, whacked} = ($urandom()); // Generate a random unsigned 32-bit value ($urandom), then cast it (2') to a 2-bit value. The inputs led, whacked are then assigned to each bit of this value (in the order: MSB to LSB).
-            // ^^^ We use the LHS concatenation operator here to efficiently assign each of the 2 inputs to a random 1-bit value.
+            {SW} = ($urandom()); // Generate a random unsigned 32-bit value ($urandom), then cast it (2') to a 2-bit value. The inputs SW, KEY are then assigned to each bit of this value (in the order: MSB to LSB).
+            {KEY[3]} = ($urandom());
+				// ^^^ We use the LHS concatenation operator here to efficiently assign each of the 2 inputs to a random 1-bit value.
             // Log input and output values:
-            $display("Inputs: \tled: %b, whacked: %b", led, whacked); // Print inputs to stdout.
+            $display("Inputs: \tSW: %b, KEY: %b", SW, KEY); // Print inputs to stdout.
             #10; // Delay for 10 time units to ensure the simulator evaluates the DUT outputs before the next line.
-            $display("Outputs:\tdisplay0: %b, display1: %b, display2: %b, display3: %b", display0, display1, display2, display3); // Print outputs to stdout.
+            $display("Outputs:\tLEDR: %b, HEX0: %b, HEX1: %b, HEX2: %b, HEX3: %b", LEDR, HEX0, HEX1, HEX2, HEX3); // Print outputs to stdout.
             $display("======================================");
             #10; // Delay for a further 10 time units to provide a total of 20 time units between input changes (as the clock period is 20 time units).
         end
